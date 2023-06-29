041cece7351fce0cca5cb2fbb1f3acb91275db6e
Nikita Khramov <xmlhttprequest404@gmail.com>
2023-06-29 18:33:28 +0400

    make popup, likes, hovers

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
-import {toggleLike} from "../blocks/element/__like/element__like.js";
+import { toggleLike } from "../blocks/element/__like/element__like.js";
+import { popupHandle } from "../blocks/popup/popup.js";
 
 
